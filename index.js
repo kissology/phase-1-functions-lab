702f89bb1b78f27e8db64cdac54b948cc5350a41
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-10-03 16:43:22 -0400

    commit tree 9ce10fa7c8d696094cc04d0bfae5395385301795

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+  return Math.abs(distance - 42);
+}
+
+function distanceFromHqInFeet(distance){
+  return distanceFromHqInBlocks(distance)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+  return (Math.abs(destination-start) * 264)
+}
+
+function calculatesFarePrice(start, destination){
+  let cost = 0;
+  if(distanceTravelledInFeet(start, destination) < 400)
+  {
+    return cost
+  }
+  else if(distanceTravelledInFeet(start, destination) < 2000)
+  {
+    return ((distanceTravelledInFeet(start, destination) - 400) * .02)
+  }
+  else if(distanceTravelledInFeet(start, destination)< 2500)
+  {
+    return 25
+  }
+  else 
+  {
+    return 'cannot travel that far'
+  }
+}
